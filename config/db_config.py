d7f9adfaae8748051fbe0158608c57a976576b48
viniciusrabello <viniciusrabello@matematica.ufrj.br>
2025-02-04 22:01:18 -0300

    added a db_config file for loading the DB_URL from my .env

---
+++ config/db_config.py
@@ -0,0 +1,10 @@
+# config/db_config.py
+
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Database connection string (PostgreSQL)
+DB_URI = os.getenv("DATABASE_URL")
