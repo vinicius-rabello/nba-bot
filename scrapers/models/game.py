1c6abb2a13df64f7cdc442e2cac115d72c885c77
viniciusrabello <viniciusrabello@matematica.ufrj.br>
2025-02-05 20:12:00 -0300

    implemented pydantic model for games validation

---
+++ scrapers/models/game.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+class Game(BaseModel):
+    game_id: str  # Hashed ID
+    full_date: str  # String field for the full date (YYYY-MM-DD)
+    game_time: str  # Game time as a string
+    broadcaster: str  # Broadcaster name as a string
+    home_team: str  # Home team name as a string
+    away_team: str  # Away team name as a string
+    arena: str  # Arena name as a string
+    city: str  # City name as a string
+    state: str  # State name as a string
--- scrapers/scraper_task.py
+++ scrapers/scraper_task.py
@@ -2,6 +2,8 @@
 from nba_scraper.nba_scraper import NbaScraper  
 # Importa a função para formatar datas
 from nba_scraper.utils import format_date, generate_game_id
+# Import a função que valida os dados
+from validation.validate_game import validate_game
 import pandas as pd  # Para manipulação de dados
 from tqdm import tqdm  # Para exibição de progresso
 from zoneinfo import ZoneInfo # Para formatação das datas
@@ -22,8 +24,10 @@ def scrape_nba_task():
         # Obtém a lista de dias disponíveis na programação
         schedule_days = scraper.get_schedule_days()
         
-        # Itera sobre os dias da programação (atualmente pegando apenas a primeiro semana [:7])
-        for schedule_day in tqdm(schedule_days[:7], desc="Processando dias"):
+        # no caso de existirem menos de 7 elementos de dia na página, pegue a quantidade restante
+        n_days = min(7, len(schedule_days))
+        # Itera sobre os dias da programação (atualmente pegando apenas a primeiro semana [:n_days])
+        for schedule_day in tqdm(schedule_days[:n_days], desc="Processando dias"):
             schedule_day_games = scraper.get_schedule_day_games(schedule_day)  # Obtém os jogos do dia
             schedule_games = scraper.get_schedule_games(schedule_day_games)  # Obtém a lista de jogos
             full_date = format_date(scraper.get_schedule_day_date(schedule_day), timezone=tz)  # Obtém e formata a data do dia
@@ -35,11 +39,32 @@ def scrape_nba_task():
                 broadcaster = scraper.get_schedule_game_broadcaster(schedule_game)
                 away_team, home_team = scraper.get_schedule_game_teams(schedule_game)
                 arena, city, state = scraper.get_schedule_game_location(schedule_game)
+
                 # Cria um Id único para a partida, baseado na data e nome dos times
                 game_id = generate_game_id(full_date, home_team, away_team)
-                
-                # Adiciona os dados do jogo à lista
-                data.append([game_id, full_date, game_time, broadcaster, home_team, away_team, arena, city, state])
+
+                # Cria um dicionário com os dados do jogo
+                game_data = {
+                    "game_id": game_id,
+                    "full_date": full_date,
+                    "game_time": game_time,
+                    "broadcaster": broadcaster,
+                    "home_team": home_team,
+                    "away_team": away_team,
+                    "arena": arena,
+                    "city": city,
+                    "state": state
+                }
+
+                # Valida os dados do jogo com o modelo Game do pydantic
+                validated_game = validate_game(game_data)
+                if validated_game:
+                    # Adiciona os dados do jogo à lista se forem válidos
+                    data.append([validated_game.game_id, validated_game.full_date, validated_game.game_time,
+                                 validated_game.broadcaster, validated_game.home_team, validated_game.away_team,
+                                 validated_game.arena, validated_game.city, validated_game.state])
+                else:
+                    print(f"Dados inválidos para o jogo: {game_data}")
 
     # Converte os dados coletados para um DataFrame do pandas
     df = pd.DataFrame(data, columns=['game_id', 'date', 'time', 'broadcaster', 'home_team', 'away_team', 'arena', 'city', 'state'])
