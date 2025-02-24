5bca1d3834235e76111a6e4df3a5b50b0209df36
viniciusrabello <viniciusrabello@matematica.ufrj.br>
2025-02-24 17:37:33 -0300

    added insert task

--- dags/get_nba_games_dag.py
+++ dags/get_nba_games_dag.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append("/opt/airflow/tasks")
 
 from tasks.scraper_task import scrape_nba_task
-# from tasks.insert_task import insert_nba_games
+from tasks.insert_task import insert_nba_games
 
 
 from airflow import DAG
@@ -20,7 +20,7 @@ with DAG (
     default_args=default_args,
     dag_id="nba_games_dag_v01",
     description="This DAG scrapes the NBA games happening at execution date.",
-    start_date=datetime.datetime(2025, 2, 20),
+    start_date=datetime.datetime(2025, 2, 24),
     schedule_interval="@daily"
 ) as dag:
     task1 = PythonOperator(
@@ -28,11 +28,11 @@ with DAG (
         python_callable=scrape_nba_task,
         op_args=["{{ ds }}"],
     )
-    # # Task 2: Insert NBA games into the database
-    # task2 = PythonOperator(
-    #     task_id="insert_task",
-    #     python_callable=insert_nba_games,
-    #     provide_context=True,  # Required to access XCom
-    # )
+    # Task 2: Insert NBA games into the database
+    task2 = PythonOperator(
+        task_id="insert_task",
+        python_callable=insert_nba_games,
+        provide_context=True,
+    )
 
-    task1 #>> task2
+    task1 >> task2
--- tasks/insert_task.py
+++ tasks/insert_task.py
@@ -2,14 +2,18 @@ import sys
 from pathlib import Path
 
 # Add the root directory to sys.path
-root_dir = Path(__file__).resolve().parent.parent  # Go up two levels to the root directory
+root_dir = Path(__file__).resolve().parent.parent
 sys.path.append(str(root_dir))
+sys.path.append("/opt/airflow/database")
 
 # database/insert_task.py
 from database.db_connection import Database
 
-def insert_nba_games(df):
+def insert_nba_games(**context):
     """Task to insert the nba games df into PostgreSQL."""
+    # Pull the DataFrame from XCom
+    ti = context['ti']
+    df = ti.xcom_pull(task_ids='scraping_task')
     db = Database()
     db.insert_or_update_nba_games(df)  # Insert/update into DB
     db.close()
